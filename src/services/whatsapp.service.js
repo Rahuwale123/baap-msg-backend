83c41625db363368dff5bdb9f17914b872ad0afd
rahul wale <staticmast@gmail.com>
2026-01-09 17:17:08 +0530

    done

--- src/services/whatsapp.service.js
+++ src/services/whatsapp.service.js
@@ -21,7 +21,15 @@ class WhatsAppService {
             }),
             puppeteer: {
                 headless: true,
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--disable-gpu'
+                ]
             }
         });
 
